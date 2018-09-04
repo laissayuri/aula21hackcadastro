465e100fa494ca761d8185ca41d181a8447d0cf3
laissayuri <40527451+laissayuri@users.noreply.github.com>
2018-09-04 10:21:37 -0300

    Com correções

--- index.js
+++ index.js
@@ -1,64 +1,79 @@
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
-const mongo = require("express-mongo-db");
-const cors = require("cors");
+const mongo = require('express-mongo-db');
+const cors = require('cors');
 
 const app = express();
 
-app.use(cors()); //aplicação pode ser acessada de qualquer lugar
-app.use(bodyParser.json()); //garante que tudo que entra e sai dessa aplicação é JSON
-app.use(mongo("mongodb://localhost/cadastro"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(mongo('mongodb://localhost/cadastro'));
 
-
-
-//[objetivo] | [método] | [endereço] | [parametros] | [resposta]
-// Obter Usuários | GET | /usuarios | nao | lista de nomes
-app.get("/usuarios", (req, res) => {
-    req.db.collection("usuarios").find().toArray((erro, dados) => {
+// - Obter os usuários | GET | /usuarios | não | lista de nomes.
+app.get('/usuarios', (req, res) => {
+    req.db.collection('usuarios').find().toArray((erro, dados) => {
         if(!erro){
-            return res.send({usuarios: dados}); // oq for escrito aqui é escrito em json
+            return res.send({usuarios: dados});
         }
         return res.status(500).send({erro: "Problema ao consultar o banco de dados"});
     });
 });
 
-// Obter 1 Usuário | GET | /usuario | nome | todos os dados do usuario
-app.get("/usuario/:nome", (req, res) => {
-    req.db.collection("usuarios").findOne({nome:req.params.nome}, (erro, dados) => {
-        if (!erro){
-            if(!dados){ //se nao tiver dados (ta vazio)
-                return res.send({}); //cria-se um objeo vazio
+// - Obter um usuário | GET | /usuario | nome | todos os dados do usuario
+
+app.get('/usuario/:nome', (req, res) => {
+    req.db.collection('usuarios').findOne({nome: req.params.nome}, (erro, dados) => {
+        if(!erro){
+            if(!dados){
+                return res.send({});
             }
             return res.send(dados);
         }
-        return res.status(500).send({erro: "Problema ao consultar o banco de dados"});
-    });
-});
-
-// Cadastrar usuarios | POST | /usuario/novo | todos os dados | true/false
-app.post("/usuario/novo", (req, res) => {
-    if (!req.body.nome || !req.body.email || !req.body.senha){ //se nao tiver o nome, o email ou a senha
-        return res.status(400).send({erro: "Parâmetros obrigatórios ausentes"});
-    }
-
-    req.db.collection("usuarios").insert(req.body, erro =>{
-        if(!erro){
-            return res.send({mensagem: "Usuário Cadastrado"});
-        }
-        return res.status(500).send({erro: "Problema ao consultar o banco de dados"});
-    });
-});
-
-// Remover Usuário | DELETE | /usuario/remover | nome | true/false;
-app.delete("usuario/remover/:nome", (req, res) => {
-    req.db.collection("usuarios").remove({nome: req.params.nome}, erro => {
-        if(!erro){
-            return res.send({mensagem: "Usuário Removido"});
-        }
-        return res.status(500).send({erro: "Problema ao consultar o banco de dados"});
+        return res.status(500).send({erro: "Problema ao consultar o banco de dados"})
     })
 });
 
-app.listen(5000, () => {
-    console.log("Serviço de Cadastro Inicializado");
+// - Cadastrar usuario | POST | /usuario/novo | todos os dados do usuario | true/false
+app.post('/usuario/novo', (req, res) => {
+    if(!req.body.nome || !req.body.email || !req.body.senha){
+        return res.status(400).send({erro: "Parâmetros obrigatórios ausentes"});
+    }
+
+    req.db.collection('usuarios').insert(req.body, erro => {
+        if(!erro){
+            return res.send({mensagem: "Usuário cadastrado"});
+        }
+        return res.status(500).send({erro: "Problema ao consultar o banco de dados"});
+    }); 
+});
+
+// - Remover usuario | DELETE | /usuario/remover | nome | true/false
+app.delete('/usuario/remover/:nome', (req, res) => {
+    req.db.collection('usuarios').remove({nome: req.params.nome}, erro => {
+        if(!erro){
+            return res.send({mensagem: "Usuário removido"});
+        }
+        return res.status(500).send({erro: "Problema ao consultar o banco de dados"});
+    });
+});
+
+app.post('/usuario/login', (req, res) => {
+    req.db.collection('usuarios').findOne({
+        nome: req.body.nome
+    }, (erro, dados) => {
+        if(!erro){
+            if(!dados){
+                return res.send({login: false});
+            }
+            if(req.body.senha === dados.senha){
+                return res.send({login: true});
+            }
+            return res.send({login: false});
+        }
+        return res.status(500).send({erro: "Sei la, não encontrado."});
+    });
+});
+
+app.listen(5000, ()=>{
+    console.log("Serviço de cadastro está sendo executado");
 });
--- script.js
+++ script.js
@@ -1,82 +1,109 @@
-let botoes = document.querySelectorAll("button");
+let botoes = document.querySelectorAll('button');
 
-//Cadastrar
+// Cadastrar
 botoes[0].onclick = function(){
-    let inputs = document.querySelectorAll("#novo input");
-    for (let input of inputs){
+    let inputs = document.querySelectorAll('#novo input');
+    for(let input of inputs){
+        if(!input.value.trim()){
+            return alert("Preencha todos os inputs");
+        }
+    }
+
+    fetch(`http://localhost:5000/usuario/${inputs[0].value.trim()}`).then(resposta => {
+        return resposta.json();
+    }).then(dados => {
+        if(dados.nome){
+            alert("Usuario já existe");
+        }
+        else{
+            let novoUsuario = {
+                nome: inputs[0].value.trim(),
+                senha: inputs[1].value.trim(),
+                email: inputs[2].value.trim(),
+            }
+        
+            fetch('http://localhost:5000/usuario/novo', {
+                method: "POST",
+                body: JSON.stringify(novoUsuario),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(resposta => {
+                return resposta.json();
+            }).then(dados => {
+                alert(dados.mensagem);
+                preencher();
+            }).catch(erro => {
+                alert("Erro ao cadastrar");
+                alert(erro);
+            });
+        }
+    }).catch(erro => {
+        alert("Erro ao cadastrar");
+        alert(erro);
+    });
+}
+
+function preencher(){
+    let lista = document.querySelector('#listadeusuarios');
+    fetch('http://localhost:5000/usuarios').then(resposta => {
+        return resposta.json();
+    }).then(dados => {
+        lista.innerHTML = JSON.stringify(dados); 
+    }).catch(erro => {
+        alert("Erro ao cadastrar");
+        alert(erro);
+    });
+}
+
+preencher();
+
+//Apagar
+botoes[1].onclick = function(){
+    let input = document.querySelector('#apagar input');
+    if(!input.value.trim()){
+        return alert("Preencha o nome do usuario");
+    }
+
+    fetch(`http://localhost:5000/usuario/remover/${input.value.trim()}`, {method: "DELETE"}).then(resposta => {
+        return resposta.json();
+    }).then(dados => {
+        alert(dados.mensagem);
+        preencher();
+    }).catch(erro => {
+        alert("Erro ao apagar usuario");
+        alert(erro);
+    });
+}
+
+botoes[2].onclick = function(){
+    let inputs = document.querySelectorAll('#testar input');
+    for(let input of inputs){
         if(!input.value.trim()){
             return alert("Preencha todos os campos");
         }
     }
 
-    fetch(`http://localhost:5000/usuario/${inputs[0].value.trim()}`, {
-
-    }).then( resposta =>{
-        return resposta.json();
-    }).then(dados =>{
-        if(dados.nome){
-            alert("Usuário já existe");
-        } else {
-            let novoUsuario = {
-                nome: inputs[0].value.trim(),
-                senha: inputs[1].value.trim(),
-                email: inputs[2].value.trim()
-            }
-        
-            fetch("http://localhost:5000/usuario/novo", {
-                method: "POST",
-                body:JSON.stringify(novoUsuario),
-                headers:{
-                    "Content-Type":"application/json"
-                }
-        
-            }).then(resposta => {
-                return resposta.json();
-            }).then(dados => {
-                alert("SUCESSO");
-                alert(dados.mensagem);
-                preencher();
-            }).catch(erro => {
-                alert("Erro ao Cadastrar");
-                alert(erro.erro);
-            });
-        }
-    }).catch(erro =>{
-        alert("Erro ao Cadastrar");
-        alert(erro);
-    });
-}
-
-
-function preencher(){
-    let lista = document.querySelector("#listadeusuarios");
-    fetch("http://localhost:5000.usuarios").then(resposta => {
-        return resposta.json();
-    }).then(dados => {
-        lista.innerHTML = JSON.stringify(dados);
-    }).catch( erro => {
-        alert("Erro ao Cadastrar");
-        alert(erro);
-    });
-}
-
-
-//Apagar
-botoes[1].onclick = function(){
-    let input = document.querySelector("#apagar input");
-    if(!input.value.trim()){
-        return alert("Preencha o nome do usuário");
-    }
-
-    fetch(`http://localhost:5000/usuario/remover/${input.value.trim()}`,{
-        method: "DELETE"
+    fetch('http://localhost:5000/usuario/login', {
+            method: "POST",
+            body: JSON.stringify({
+                nome: inputs[0].value,
+                senha: inputs[1].value
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }            
     }).then(resposta => {
-        return resposta.json();
+        return resposta.json()
     }).then(dados => {
-        console.log(dados.mensagem);
-        preencher();
+        if(dados.login){
+            alert("Sucesso");
+        }
+        else{
+            alert("Fracasso");
+        }
     }).catch(erro => {
-        alert("Erro ao apagar o usário");
+        alert("Erro ao consultar o login");
         alert(erro);
     });
 }
